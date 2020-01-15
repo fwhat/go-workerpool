ad9fa69c4d49425c4e4f7efdd5714e9800b41ddb
千珏 <admin@dowte.com>
2020-01-15 14:00:16 +0800

    1. 拓展job的功能

--- examples/run.go
+++ examples/run.go
@@ -9,27 +9,34 @@ import (
 )
 
 type Job struct {
-	Id int
+	Id      int
+	Process int
 }
 
-func (job Job) Run(worker go_workerpool.Worker) {
+func (job *Job) Run(worker go_workerpool.Worker) {
 	rand.Seed(time.Now().UnixNano())
-	time.Sleep(time.Second * time.Duration(rand.Intn(10)))
-	log.Printf("job %d: 执行完成", job.Id)
+	total := rand.Intn(10) + 10
+	for i := 0; i < rand.Intn(10); i++ {
+		time.Sleep(time.Second * time.Duration(i))
+		job.Process = i * 100 / total
+	}
+	job.Process = 100
 }
 
-func addQueue(dispatcher *go_workerpool.Dispatcher) {
+func addQueue(dispatcher *go_workerpool.Dispatcher) (jobs []*Job) {
 	for i := 0; i < 20; i++ {
 		// 新建一个任务
 		job := Job{Id: i}
 		// 任务放入任务队列channel
-		success := dispatcher.TryEnqueue(job)
-		if success {
+		if dispatcher.TryEnqueue(&job) {
+			jobs = append(jobs, &job)
 			log.Printf("job %d: 加入JobQueue队列 成功", job.Id)
 		} else {
 			log.Printf("job %d: 加入JobQueue队列 失败", job.Id)
 		}
 	}
+
+	return jobs
 }
 
 func main() {
@@ -47,9 +54,17 @@ func main() {
 		}
 	}(dispatcher)
 	time.Sleep(1 * time.Second)
-	go addQueue(dispatcher)
-	time.Sleep(50 * time.Second)
-	go addQueue(dispatcher)
+	jobs := addQueue(dispatcher)
+
+	go func(jobs []*Job) {
+		for {
+			for _, job := range jobs {
+				log.Printf("job: %d, process %d", job.Id, job.Process)
+			}
+			time.Sleep(2 * time.Second)
+		}
+	}(jobs)
+
 	time.Sleep(1000 * time.Second)
 
 	// 最后协程数: main进程1, 定时统计1, 主调度1协程, worker ${MaxWorker} = 8;
