a8c39a48d721f67f3cb1ecd5a4db145d3cb816f6
千珏 <admin@dowte.com>
2020-01-15 11:07:56 +0800

    1. sample worker-pool

---
+++ workerpool.go
@@ -0,0 +1,106 @@
+package go_workerpool
+
+type Job interface {
+	Run(worker Worker)
+}
+
+// execute process
+type Worker struct {
+	OnSuccessChannel chan Worker // worker执行完成通知 Channel
+	JobChannel       chan Job    // 新job分配 Channel
+	quit             chan bool   // 退出信号
+	no               int         // worker编号
+}
+
+//创建一个新worker
+func NewWorker(successChannel chan Worker, no int) Worker {
+	return Worker{
+		OnSuccessChannel: successChannel,
+		JobChannel:       make(chan Job),
+		quit:             make(chan bool),
+		no:               no,
+	}
+}
+
+//循环  监听任务和结束信号
+func (w Worker) Start() {
+	go func() {
+		for {
+			select {
+			case job := <-w.JobChannel:
+				// 收到新的job任务 开始执行
+				job.Run(w)
+				w.OnSuccessChannel <- w
+				return
+			case <-w.quit:
+				// 收到退出信号
+				w.OnSuccessChannel <- w
+				return
+			}
+		}
+	}()
+}
+
+// 停止信号
+func (w Worker) Stop() {
+	go func() {
+		w.quit <- true
+	}()
+}
+
+//调度中心
+type Dispatcher struct {
+	// 空闲的worker
+	FreeWorkers chan Worker
+	// 所有worker实例
+	Workers []Worker
+	// 等待执行的job
+	PendingJobs chan Job
+}
+
+//创建调度中心
+func NewDispatcher(maxWorkers int, maxPendingJobs int) *Dispatcher {
+	return &Dispatcher{FreeWorkers: make(chan Worker, maxWorkers), PendingJobs: make(chan Job, maxPendingJobs)}
+}
+
+//工作者池的初始化
+func (d *Dispatcher) Run() {
+	for i := 1; i < cap(d.FreeWorkers)+1; i++ {
+		worker := NewWorker(d.FreeWorkers, i)
+		worker.Start()
+
+		d.FreeWorkers <- worker
+		d.Workers = append(d.Workers, worker)
+	}
+	go d.dispatch()
+}
+
+//调度
+func (d *Dispatcher) dispatch() {
+	for {
+		select {
+		// 得到空闲worker
+		case worker := <-d.FreeWorkers:
+			go func(worker Worker) {
+				for {
+					select {
+					case job := <-d.PendingJobs:
+						// 得到 pending 中的job 并 通知worker开始执行
+						worker.JobChannel <- job
+						return
+					}
+				}
+
+			}(worker)
+		}
+	}
+}
+
+func (d *Dispatcher) TryEnqueue(job Job) bool {
+	select {
+	case d.PendingJobs <- job:
+		return true
+	default:
+		return false
+	}
+}
